c01225f6d78767cb76b412d3bb5afc9134627cf6
HonestCo <ArickTheDisciple@gmail.com>
2024-03-01 18:42:23 -0700

    Some writing code for files

--- Wcommands.py
+++ Wcommands.py
@@ -1,6 +1,14 @@
 class Wcommands:
-    def __init__(self, command_type, commands):
-        self.com_type = command_type
+    def __init__(self, commandType, commands):
+        self.com_type = commandType
         self.commands = commands
-        
+
+    def write_file(self, fileName, writeType, commandType, commands):
+        with open(fileName, writeType, encoding='utf-8') as open_file:
+            open_file.write('BREAK BREAK\n')
+            command_line = commandType
+            for com in commands:
+                command_line += f" {com}"
+            open_file.write(command_line + '\n')
+
 
