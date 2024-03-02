386cf67db585e62aac43f6c18fd78f3a46b8690d
HonestCo <ArickTheDisciple@gmail.com>
2024-03-02 03:14:27 -0700

    Added undo and temp side bar

--- CAD.py
+++ CAD.py
@@ -28,8 +28,35 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 running = True
 
 def super_UI():
-    topBar = pygame.Rect(0, 0, 500, 70)
+    #Side bar brushes with shapes 
+    sideBar = pygame.Rect(0,0, 120, SCREEN_HEIGHT)
+    pygame.draw.rect(screen, (180,190,200), sideBar)
+
+    linebox = pygame.image.load('.\\Draw_Bot\\images\\line.png')
+    screen.blit(linebox, (30, SCREEN_HEIGHT / 6))
+
+    boxbox = pygame.image.load('.\\Draw_Bot\\images\\square.png')
+    screen.blit(boxbox, (30, (SCREEN_HEIGHT / 6)*2))
+
+    circlebox = pygame.image.load('.\\Draw_Bot\\images\\circle.png')
+    screen.blit(circlebox, (30, (SCREEN_HEIGHT / 6)*3))
+
+    arcbox = pygame.image.load('.\\Draw_Bot\\images\\arc.png')
+    screen.blit(arcbox, (30, (SCREEN_HEIGHT / 6)*4))
+
+    starbox = pygame.image.load('.\\Draw_Bot\\images\\star.png')
+    screen.blit(starbox, (30, (SCREEN_HEIGHT / 6)*5))
+
+
+    #Top Bar UI With buttons
+    topBar = pygame.Rect(0, 0, SCREEN_WIDTH, 70)
     pygame.draw.rect(screen, (255,255,255), topBar)
+
+    imgpy = pygame.image.load('.\\Draw_Bot\\images\\bar.png').convert()
+    imgpy = pygame.transform.scale(imgpy, (SCREEN_WIDTH, 70))
+    screen.blit(imgpy, (0,0))
+
+
     new_screen_but_bord = pygame.Rect(10, 30, 230, 30)
     pygame.draw.rect(screen, (200,200,200), new_screen_but_bord)
     save_drawing_but_bord = pygame.Rect(260, 30, 230, 30)
@@ -47,13 +74,18 @@ def clearLines(players):
     players.currentNodes = []
     shapes_list.clear()
 
+def doubPop(players):
+    if len(player.currentNodes) > 1:
+        players.currentNodes.pop()
+        players.currentNodes.pop()
+
     #Actual buttons 
 new_screen_but = Button(
         #Following documentation
         screen,  # Surface to place button on
         10,  # X-coordinate of top left corner
         30,  # Y-coordinate of top left corner
-        230,  # Width
+        (SCREEN_WIDTH / 3) -10,  # Width
         30,  # Height
 
         # Optional Parameters
@@ -69,9 +101,9 @@ new_screen_but = Button(
     
 export_screen_but = Button(
         screen,  # Surface to place button on
-        260,  # X-coordinate of top left corner
+        (SCREEN_WIDTH / 3) + 5,  # X-coordinate of top left corner
         30,  # Y-coordinate of top left corner
-        230,  # Width
+        (SCREEN_WIDTH / 3) - 10,  # Width
         30,  # Height
 
         # Optional Parameters
@@ -84,6 +116,26 @@ export_screen_but = Button(
         radius=20,  # Radius of border corners (leave empty for not curved)
         onClick=lambda: GenCommands.reciever(shapes_list)  # Function to call when clicked on
     )
+
+undo_screen_but = Button(
+        screen,  # Surface to place button on
+        2*(SCREEN_WIDTH / 3),  # X-coordinate of top left corner
+        30,  # Y-coordinate of top left corner
+        (SCREEN_WIDTH/ 3) - 10,  # Width
+        30,  # Height
+
+        # Optional Parameters
+        text='Undo',  # Text to display
+        fontSize=20,  # Size of font
+        margin=20,  # Minimum distance between text/image and edge of button
+        inactiveColour=(150,150,150),  # Colour of button when not being interacted with
+        hoverColour=(100, 150, 150),  # Colour of button when being hovered over
+        pressedColour=(100, 200, 200),  # Colour of button when being clicked
+        radius=20,  # Radius of border corners (leave empty for not curved)
+        onClick=lambda: doubPop(player) # Function to call when clicked on
+    )
+
+
 
 class UserPointer():
     def __init__(self):
