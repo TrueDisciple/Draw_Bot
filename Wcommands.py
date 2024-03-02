33f17d7b62ab3a40ac66fbe19e227e0487c413ea
HonestCo <ArickTheDisciple@gmail.com>
2024-03-02 13:52:20 -0700

    Arrays form

--- Wcommands.py
+++ Wcommands.py
@@ -4,16 +4,16 @@ class Wcommands:
 
     def write_file(self, fileName, writeType, commands):
         with open(fileName, writeType, encoding='utf-8') as open_file:
-            open_file.write('BREAK BREAK\n')
+            open_file.write('[')
             for shape in commands:
                 prevPos = None
-                open_file.write(f"LINE {round(shape[0][0])} {round(shape[0][1])}\n")
-                open_file.write("PENDOWN\n")
+                open_file.write(f"LINE {round(shape[0][0])} {round(shape[0][1])},\n")
+                open_file.write("PENDOWN,\n")
                 for pos in shape:
                     if prevPos:
-                        open_file.write(f'LINE {round(pos[0])} {round(pos[1])}\n')
+                        open_file.write(f'LINE {round(pos[0])} {round(pos[1])},\n')
                     prevPos = pos
-                open_file.write("PENUP\n")
-            open_file.write("END")
+                open_file.write("PENUP,")
+            open_file.write("]")
             open_file.close()
 
