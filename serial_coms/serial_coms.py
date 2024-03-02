2c8a04e1cea38d73523b1224a54ba4dd8d41051b
dylan <136660038+Throarin@users.noreply.github.com>
2024-03-01 21:44:09 -0700

    Update serial_coms.py

--- serial_coms/serial_coms.py
+++ serial_coms/serial_coms.py
@@ -4,13 +4,17 @@
 import serial
 from time import sleep
 
+# command_file = open("filepath", "r").readlines
+
+
+
+
 if __name__ == '__main__':
     ser = serial.Serial('COM4', 9600, timeout=1)
     ser.reset_input_buffer()
 
     while True:
-        ser.write(b"Hello from Erebor!\n")
+        ser.write(b"LINE 24 35 46 57")
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
         sleep(1)
-
