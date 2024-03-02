fff8fa5b763ec29eca627d605fb4f32bdec33fe2
jseffker <jseffker@gmail.com>
2024-03-01 20:39:17 -0700

    Line drawing done

--- CAD.py
+++ CAD.py
@@ -6,6 +6,8 @@ from pygame.locals import (
     K_RIGHT,
     K_ESCAPE,
     KEYDOWN,
+    MOUSEBUTTONDOWN,
+    MOUSEBUTTONUP,
     QUIT,
 )
 
@@ -13,26 +15,63 @@ pygame.init()
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
+PX_PER_IN = 20
 
+shapes_list = []
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 running = True
 
-def processInput():
-    global running
+class UserPointer():
+    def __init__(self):
+        self.pos = pygame.mouse.get_pos()
+        self.clicking = False
+        self.currentNodes = []
+
+    def set_pos(self, mouse_pos):
+        self.pos = mouse_pos
+
+player = UserPointer()
+
+while running:
+    # process input
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
         elif event.type == QUIT:
             running = False
+        if event.type == MOUSEBUTTONDOWN:
+            player.clicking = True
+            if len(player.currentNodes) == 0:
+                player.currentNodes.append(pygame.mouse.get_pos())
+            else:
+                if not player.currentNodes[len(player.currentNodes)-1] == pygame.mouse.get_pos():
+                    player.currentNodes.append(pygame.mouse.get_pos())
+                else:
+                    shapes_list.append(player.currentNodes)
+                    player.currentNodes = []
 
-def update():
-    pass
+        if event.type == MOUSEBUTTONUP:
+            player.clicking = False
 
-def render():
-    pass
+    # update code
+    player.set_pos(pygame.mouse.get_pos())
 
-while running:
-    processInput()
-    update()
-    render()
+    # render
+    screen.fill((0, 0, 50))
+    # i, j = 0, 0
+    # while i < SCREEN_WIDTH:
+    #     while j < SCREEN_HEIGHT:
+    #         pass
+
+    pygame.draw.circle(
+        screen,
+        (255, 0, 0) if player.clicking else (255, 255, 255),
+        player.pos,
+        7
+    )
+    if(len(player.currentNodes) >= 2):
+        pygame.draw.lines(screen, (255, 255, 255), False, player.currentNodes, 10)
+    for shape in shapes_list:
+        pygame.draw.lines(screen, (255, 255, 0), False, shape, 10)
+    pygame.display.flip()
